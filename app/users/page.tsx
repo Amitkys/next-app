5b04c15d99156f8e4c585051b1845811c1db0779
KYs <amitkys59@gmail.com>
2024-10-04 20:49:10 +0530

    added axios

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,16 @@
-import { PrismaClient } from "@prisma/client/"
-export default function() {
+import { PrismaClient } from "@prisma/client/";
+const client = new PrismaClient();
+
+async function fetchData() {
+    const all_users = await client.user.findMany();
+    console.log(all_users);
+}
+
+export default function () {
+    fetchData();
     return (
         <div>
-            
+            <h1>this is user page</h1>
         </div>
     )
 }
