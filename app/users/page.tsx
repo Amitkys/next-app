042996714bca1c9dc8bd88efb5f244dc80298e6b
KYs <amitkys59@gmail.com>
2024-10-04 20:43:21 +0530

    added missing file or content

---
+++ app/users/page.tsx
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client/"
+export default function() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ learning/axios_fetch_todo_data.tsx
@@ -0,0 +1,19 @@
+async function getData() {
+    const res = await axios.get('https://backend-ddtz.onrender.com/todos');
+    return res.data;
+  }
+  export default async function Home() {
+    const data = await getData();
+    return (
+      <div>
+        <h1>this is home page</h1>
+        {data.all_todo.map((todo: any) => (
+          <div key={todo._id}>
+            <h2>Title: {todo.title}</h2>
+            <p>Description: {todo.description} </p>
+          </div>
+        ))} 
+        <Link href={'/testing'}>go to testing</Link>
+      </div>
+    )
+  }
