b3f75bbbc2c82eb93c84613206c5e3ffa6ec9a9f
KYs <amitkys59@gmail.com>
2024-10-04 23:04:04 +0530

    added singleton prisma

---
+++ app/users/[userId]/page.tsx
@@ -0,0 +1,19 @@
+import client from "@/lib/Singleton_prisma";
+
+async function fetchData(id: string) {
+
+    const userId = parseInt(id);
+
+    const user = await client.user.findUnique({
+        where: { id: userId },
+    });
+    return user;
+}
+export default async function User({ params }: { params: { userId: string } }) {
+    const data = await fetchData(params.userId);
+    return (
+        <div>
+            {/* <h1>This is user ID page {params.userId} </h1> */}
+        </div>
+    );
+}
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,6 @@
-import { PrismaClient } from "@prisma/client";
-import Link from "next/link";
 
-const client = new PrismaClient();
+import Link from "next/link";
+import client from "@/lib/Singleton_prisma";
 
 async function fetchData() {
     const all_users = await client.user.findMany();
@@ -17,7 +16,7 @@ export default async function () {
                     <p>{user.username} </p>
                     <p>{user.firstName} </p>
                     <p>{user.lastName} </p>
-                    <Link href={`/user/${user.id}`}><button>edit</button></Link>
+                    <Link href={`/users/${user.id}`}><button>edit</button></Link>
                     <hr />
                 </div>
             ))}
